0491c74a45566679a0fc02970bd86a6f219f6397
Chris P <chrisp1877@gmail.com>
2020-11-20 07:48:55 -0800

    a lot of work on making a semi functional grammar tree come out of the parser, maybe unnecessarily

--- di-parse/src/lib.rs
+++ di-parse/src/lib.rs
@@ -1,5 +1,4 @@
-#[macro_use]
-extern crate pest_derive;
+#[macro_use] extern crate pest_derive;
 
 use pest::Parser;
 
@@ -20,12 +19,12 @@ use pest::{
 };
 use parse::DivParser;
 
-fn main() {
+pub fn run() {
     let filename = env::args().nth(1)
         .unwrap_or("test.txt".to_string());
     let mut file = File::open(filename).expect("File doesn't exist");
     let mut txtbuf = String::new();
     file.read_to_string(&mut txtbuf).expect("Error reading file");
     println!("{:?}", txtbuf);
-    DivParser::get_pairs(txtbuf);
+    DivParser::get_pairs(txtbuf).unwrap();
 }
